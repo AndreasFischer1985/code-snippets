df512a7bd1240cf0bb09e280d2d1c160b1d9c4e2
Dr. Andreas Fischer <45405681+AndreasFischer1985@users.noreply.github.com>
2022-01-09 14:50:21 +0100

    Add files via upload

--- threeD.R
+++ threeD.R
@@ -410,6 +410,40 @@ barplot3D=function(x,col=NULL,transparency=.2,xlim=NULL,ylim=NULL,main="3D Barpl
     if(add.numbers==T)text(x1,y1,v1,srt=txt.srt,pos=txt.pos,xpd=T)
     return(list(x=x1,y=y1,value=v1))
 }
+surfaceplot3D=function(x=-10:10,y=-10:10,fun=NULL,dim=c(10,10),border=NA,col=rgb(0,0,1),transparency=.1,thetaX=10,thetaY=60,thetaZ=10,culling="none",add.wireframe=T,add.polygons=T,...){
+    if(is.null(fun))fun=function(x,y)x*y
+    x=rep(seq(min(x),max(x),length.out=dim[1]),times=dim[2])
+    y=rep(seq(min(y),max(y),length.out=dim[2]),each=dim[2])
+    z=fun(x,y)
+    ver=vecToVer(x,z,y)
+    pol=numeric(0);
+    con=numeric(0);
+    for(j in 1:(dim[2]-1)){
+        for(i in 1:(dim[1]-1)){ #(dim[1]*dim[2]-dim[1]-1)){
+            pol=c(pol,
+                    (dim[1]*(j-1))+i+0,(dim[1]*(j-1))+i+dim[1]+0,(dim[1]*(j-1))+i+dim[1]+1, 
+                    (dim[1]*(j-1))+i+0,(dim[1]*(j-1))+i+dim[1]+1,(dim[1]*(j-1))+i+1)
+            con=c(con,
+                    (dim[1]*(j-1))+i,(dim[1]*(j-1))+i+1,
+                    (dim[1]*(j-1))+i+1,(dim[1]*(j-1))+i+dim[1]+1,
+                    (dim[1]*(j-1))+i+dim[1]+1,(dim[1]*(j-1))+i+dim[1]+0,
+                    (dim[1]*(j-1))+i+dim[1]+0,(dim[1]*(j-1))+i
+                    )
+        }
+    }
+    ver[seq(1,length(ver),by=3)+0]=(ver[seq(1,length(ver),by=3)+0])/(max(ver[seq(1,length(ver),by=3)+0]))
+    ver[seq(1,length(ver),by=3)+1]=(ver[seq(1,length(ver),by=3)+1])/(max(ver[seq(1,length(ver),by=3)+1]))
+    ver[seq(1,length(ver),by=3)+2]=(ver[seq(1,length(ver),by=3)+2])/(max(ver[seq(1,length(ver),by=3)+2]))
+    rver=unitRver()
+    cube=form("cube")
+    tver=transformRver(rver,ver,thetaX=thetaX,thetaY=thetaY,thetaZ=thetaZ)
+    tver2=transformRver(rver,cube$ver,thetaX=thetaX,thetaY=thetaY,thetaZ=thetaZ)
+    plotWireframe(tver2,cube$con,add=F,subset="back")
+    if(add.polygons)plotPolygons(tver,pol,border=border,add=T,col=col,culling=culling,transparency=transparency,...)
+    if(add.wireframe)plotWireframe(tver,con,add=T,col=c(col,"black"))
+    plotWireframe(tver2,form("cube")$con,add=T,subset="front")
+    invisible(list("ver"=ver,"pol"=pol,"con"=con))
+}
 
 
 ######################
@@ -451,7 +485,7 @@ create(package.name,
 description=list(
 "Package"="threeD",
 "Title"="Basic functions for plotting and manipulating 3D-Objects based on R's default packages.",
-"Version"="0.0.3",
+"Version"="0.0.4",
 "Authors@R"="person(\"Andreas\", \"Fischer\", email = \"andreasfischer1985@web.de\", role = c(\"aut\", \"cre\"))",
 "Maintainer"="'Andreas Fischer' <andreasfischer1985@web.de>",
 "Description"=
@@ -490,6 +524,9 @@ devtools::install(package.name);
 ####################
 
 library(threeD)
+
+dev.new();
+f=surfaceplot3D(fun=function(x,y)x*y);#f
 
 x=cbind(c(1,2,3),c(2,2,2),c(3,2,1))
 dev.new();
