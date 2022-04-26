8e8223bc831632583a8a17dd603e7dff1b8d67f6
Dr. Andreas Fischer <45405681+AndreasFischer1985@users.noreply.github.com>
2022-04-26 21:27:02 +0200

    Rename chatBot.txt to chatBot.R

--- chatBot.R
+++ chatBot.R
@@ -43,7 +43,7 @@ chatBot=function(brain=NULL,data=data.frame(x=c(1,2,3),y=c(2,2,3))){
 		inp=gsub("[,;]","",tolower(inp))
 
 		# Sonderfunktionen:
-		if(length(inp)==0|is.na(inp)){if(is.null(brain)) return("Kannst du mir sagen, wo ich meinen Kopf gelassen habe?") else return("Herzlich willkommen, was kann ich für dich tun?")} else
+		if(length(inp)==0|is.na(inp)){if(is.null(brain)) return("Kannst du mir sagen, wo ich meinen Kopf gelassen habe?") else return("Herzlich willkommen, was kann ich fÃ¼r dich tun?")} else
 		if(matches("^Kannst du mir sagen, wo ich meinen Kopf gelassen habe[?]$",out2)) {
 			if(file.exists(inp)) return("[Brain-Datei laden]")
 			if(matches("^[Nn]ein",inp))  return("Okay, dann arbeite ich weiter im Default-Modus.") 
@@ -57,9 +57,9 @@ chatBot=function(brain=NULL,data=data.frame(x=c(1,2,3),y=c(2,2,3))){
 		}
 
 		# Reaktionen auf Aufforderungen:
-		x="(^[Uu]hrzeit$|[Ww]ie spät ist es|wie spät es ist)";if(matches(x,inp)) return(paste0("Wir haben jetzt ",Sys.time()))
+		x="(^[Uu]hrzeit$|[Ww]ie spÃ¤t ist es|wie spÃ¤t es ist)";if(matches(x,inp)) return(paste0("Wir haben jetzt ",Sys.time()))
 		x="(^[Ww]etter|^[Ww]etterbericht)";if(matches(x,inp)) return(paste0("[Wetter berichten]"))
-		x="(^[Hh]umor|Erzähl einen Witz)";if(matches(x,inp)){
+		x="(^[Hh]umor|ErzÃ¤hl einen Witz)";if(matches(x,inp)){
 			html=paste(readLines("https://www.aberwitzig.com/"), collapse = "\n")
 			return(gsub("(<.*?>|&nbsp;|&quot;|\n|\t)","",gsub("(.*\t2[.]|\t3[.].*)","",html)))
 		}
@@ -73,15 +73,15 @@ chatBot=function(brain=NULL,data=data.frame(x=c(1,2,3),y=c(2,2,3))){
 		x="(^[Vv]ideo[s]?|^[Ss]spiel [Mm]Video[s]?) ";if(matches(x,inp)){
 			return(paste0("[Video abspielen]"))
 		}
-		x="^([Öö]ffne|[Bb]rowse) ";if(matches(x,inp)){
+		x="^([Ã–Ã¶]ffne|[Bb]rowse) ";if(matches(x,inp)){
 			tryCatch(
-			{path=sub("^([Öö]ffne|[Bb]rowse) ","",inp0); if(grepl("(http|www[.])",path)[1]==F) path=paste0("www.",path); browseURL(path);return(paste0("[Web-Browser gestartet]"));},
+			{path=sub("^([Ã–Ã¶]ffne|[Bb]rowse) ","",inp0); if(grepl("(http|www[.])",path)[1]==F) path=paste0("www.",path); browseURL(path);return(paste0("[Web-Browser gestartet]"));},
  			error=function(cond) {
-				try({shell(sub("^([Öö]ffne|[Bb]rowse) ","",inp0), wait=FALSE);return(paste0("[File-Browser gestartet]"));})
-				return(paste0("Ich konnte ",sub("^([Öö]ffne|[Bb]rowse) ","",inp0)," nicht öffnen"))
+				try({shell(sub("^([Ã–Ã¶]ffne|[Bb]rowse) ","",inp0), wait=FALSE);return(paste0("[File-Browser gestartet]"));})
+				return(paste0("Ich konnte ",sub("^([Ã–Ã¶]ffne|[Bb]rowse) ","",inp0)," nicht Ã¶ffnen"))
 			}
 			)
-			return(paste0("Ich konnte die Website ",sub("^([Öö]ffne|[Bb]rowse) ","",inp0)," nicht öffnen"))
+			return(paste0("Ich konnte die Website ",sub("^([Ã–Ã¶]ffne|[Bb]rowse) ","",inp0)," nicht Ã¶ffnen"))
 		}
 		x="^([Zz]eige|[Pp]lotte) ";if(matches(x,inp)){ 
 			if(matches("[0-9]+",inp)) {
@@ -89,7 +89,7 @@ chatBot=function(brain=NULL,data=data.frame(x=c(1,2,3),y=c(2,2,3))){
 				indices=as.numeric(unlist(matchAll(inp,"[0-9]+",value=T)));print(indices)
 				if(length(indices)==1 & indices[1]>0 & indices[1]<=dim(data)[2]) { hist(data[,indices[1]],xlab=indices[1],main="Histogram");plotted=T} else 
 				if(length(indices)==2 & indices[1]>0 & indices[2]>0 & indices[1]<=dim(data)[2] & indices[2]<=dim(data)[2]) { plot(data[,indices[1]],data[,indices[2]],xlab=indices[1],ylab=indices[2]); plotted=T} 
-				if(length(indices)>2) return("Plots von mehr als 2 Variablen überfordern mich aktuell leider noch.")	
+				if(length(indices)>2) return("Plots von mehr als 2 Variablen Ã¼berfordern mich aktuell leider noch.")	
 				if(plotted==T)return(paste0("Daten mit Index ",paste(indices,collapse=",")," geplottet."))
 				if(plotted==F)return(paste0("Daten mit Index ",paste(indices,collapse=",")," konnte ich nicht plotten."))
 			}
@@ -105,19 +105,19 @@ chatBot=function(brain=NULL,data=data.frame(x=c(1,2,3),y=c(2,2,3))){
 		if(matches("^Warum glaubst du denn ",out2)) return("Ach so.") 
 		
 		# Allgemeine Reaktionen auf Fragen:
-		x="([Ww]er ist |[Ww]as ist |[Ee]rzähl mir von )";if(matches(x,inp)) return(paste0("Willst du eine Google-Suche nach ",star(x,inp)," starten?"))
+		x="([Ww]er ist |[Ww]as ist |[Ee]rzÃ¤hl mir von )";if(matches(x,inp)) return(paste0("Willst du eine Google-Suche nach ",star(x,inp)," starten?"))
 		x="([Ww]er hat )";if(matches(x,inp)) return(paste0("Willst du eine Google-Suche danach starten, wer ",star(x,inp)," hat?"))
 		x="([Ww]arum|[Ww]eshalb|[Ww]wozu|[Ww]weswegen)";if(matches(x,inp)) return(paste0("Ich kenne den Grund leider auch nicht."))
-		x="([Ww]ie|[Ww]oher|[Gg]ibt es|[?])";if(matches(x,inp)) return(paste0("Das weiß ich leider auch nicht."))
-		x="([Kk]annst du|[Hh]ast du)";if(matches(x,inp)) return(paste0("Das weiß ich leider auch nicht."))
-		x="([Ww]ürdest|[Mm]öchtest|[Ww]illst|[Ww]irst) du";if(matches(x,inp)) return(paste0("Ich bin mir nicht sicher, ob ich das wünschenswert finde."))
+		x="([Ww]ie|[Ww]oher|[Gg]ibt es|[?])";if(matches(x,inp)) return(paste0("Das weiÃŸ ich leider auch nicht."))
+		x="([Kk]annst du|[Hh]ast du)";if(matches(x,inp)) return(paste0("Das weiÃŸ ich leider auch nicht."))
+		x="([Ww]Ã¼rdest|[Mm]Ã¶chtest|[Ww]illst|[Ww]irst) du";if(matches(x,inp)) return(paste0("Ich bin mir nicht sicher, ob ich das wÃ¼nschenswert finde."))
 
 		# Allgemeine Reaktionen auf Aussagen:
 		x="[Ii]ch bin ";if(matches(x,inp)) return(paste0("Warum glaubst du denn ",star(x,inp)," zu sein?"))
 		x="[Ii]ch (denke|glaube|meine) ([Aa]ber )?";if(matches(x,inp)) return(paste0("Warum glaubst du denn ",star(x,inp),"?"))
 		x="[Ii]ch (kann) ([Aa]ber )?";if(matches(x,inp)) return(paste0("Warum kannst du ",star(x,inp),"?"))
 		x="[Ii]ch (habe) ([Aa]ber )?";if(matches(x,inp)) return(paste0("Warum hast du denn ",star(x,inp),"?"))
-		x="[Ii]ch (würde) ([Aa]ber )?";if(matches(x,inp)) return(paste0("Warum würdest du denn ",star(x,inp),"?"))
+		x="[Ii]ch (wÃ¼rde) ([Aa]ber )?";if(matches(x,inp)) return(paste0("Warum wÃ¼rdest du denn ",star(x,inp),"?"))
 		x="[Ww](eil|egen)";if(matches(x,inp)) return(paste0("Ach deswegen."))
 		x="([Aa]ber )?[Dd]as ist( aber)?";if(matches(x,inp)) return(paste0("Ja, ich denke auch das ist",star(x,inp)))
 
