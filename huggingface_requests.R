87e69c88fe812b3f0abe2e45e98a7e10fc28d446
Dr. Andreas Fischer <45405681+AndreasFischer1985@users.noreply.github.com>
2023-02-18 13:54:16 +0100

    Update huggingface_requests.R

--- huggingface_requests.R
+++ huggingface_requests.R
@@ -32,6 +32,15 @@ httr::content(post)
 #post=$(curl -X POST $url -H "Content-Type: application/json" --data-raw "$data")
 #echo $post
 
+##!/bin/bash
+#model_id="bigscience/bloom" 
+#payload="The following email explains two things:\n1) The writer, Andy, is going to miss work.\n2)The receiver, Betty, is Andy's boss and can email if #anything needs to be done.\n\nFrom:"
+#params='{"max_new_tokens": 200, "return_full_text":false}'
+#url="https://api-inference.huggingface.co/models/"$model_id
+#data='{"inputs":"'$payload'","parameters":'$params'}'
+#post=$(curl -X POST $url -H "Content-Type: application/json" --data-raw "$data")
+#echo $post
+
 
 # Diffusers
 #------------------
@@ -99,8 +108,29 @@ bot=function(
   httr::content(post)
 }
 
+
+# Selected resonses:
+#--------------------
+
+bot("google/flan-t5-large")
+# Output of flan-t5-large:
+# "Andy: I'm going to miss work. Can you email me if anything needs to be done?"
+
+bot("bigscience/bloom",
+	params=list("max_new_tokens"=200,"num_beams"=16, "early_stopping"=TRUE))
+# Output of bloom:
+# "The following email explains two things:\n1) The writer, Andy, is going to miss work.\n2) The receiver, Betty, is Andy's boss and can email if anything needs to be done.\n\nFrom: Andy <andy@company.com>\nTo: Betty <betty@company.com>\nSubject: I am going to miss work tomorrow\nDate: Mon, 1 Feb 2016 10:45:00 +0000\n\nI am going to miss work tomorrow. I have a doctor's appointment. I will be\n  back on Tuesday.\n\nA:\n\nI am going to miss work tomorrow. I have a doctor's appointment. I\n  will be back on Tuesday.\n\nThis is a perfectly fine way to write an email. It is clear, concise, and to the point. It is also a good example of how to write an email in the future tense."
+
+bot("EleutherAI/gpt-j-6B",
+	params=list("max_length"=300, "length_penalty"=2, "num_beams"=1, "early_stopping"=TRUE,"return_full_text"=FALSE))
+#" Andy  <andy_sales@hotmail.com>\nDate: Mon, 2 Oct 2000 19:26:32 -0400\n>Date: Sunday, October 01, 2000 10:41:47 PM\n>To: Betty Smith <ladybarb@hotmail.com>\n> Subject: Fwd: Fw: [IMG]\n>\n>\n>\n>>\n>>\n>>From: Andy Tichy <andy_sales@hotmail.com>\n>>To: \"Betty & John\" <bettyandjohn@hotmail.com>, \"Beth\"\n>> <barb.tichy@fas.com>, \"Betty & John\" <bettyandjohn@hotmail.com>,\n>>\"Butterball\" <b@r-tech.net>, \"Bun\" <bonaduce@hotmail.com>, \"C. & V\"\n>><candysalecat@hotmail.com>, \"Carol\" <c@cs.com>, \"Charlie\"\n>><charlie_sales@hotmail.com>, \"Chloe & John\" <mwatson6@"
+
+
+# Compare several models: 
+#------------------------
+
 working_ids=c(1,2,5,10,11,15,16,18)
-#4: num_beams=16 too much for CUDA
+#4: num_beams too much for CUDA
 #12,14: no parameters allowed (Internal Server Error without parameters)
 #17: model overloaded
 #other: model time out
@@ -111,9 +141,3 @@ for(i in working_ids){
 }
 
 
-# Inspect response of flan-t5-large:
-#-----------------------------------
-
-bot(model_ids[1])
-# Output of flan-t5-large:
-# "Andy: I'm going to miss work. Can you email me if anything needs to be done?"
