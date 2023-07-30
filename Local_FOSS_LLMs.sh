a501eaeae654ff154844514f431fde33cf5e2bcc
Dr. Andreas Fischer <45405681+AndreasFischer1985@users.noreply.github.com>
2023-07-30 13:52:56 +0200

    Add SDXL

--- Local_FOSS_LLMs.sh
+++ Local_FOSS_LLMs.sh
@@ -237,6 +237,58 @@ else:
 '|python
 
 
+# SDXL - test Diffusers
+#-----------------------
+echo '
+from diffusers import StableDiffusionXLPipeline
+import torch
+import gc
+pipe = StableDiffusionXLPipeline.from_pretrained(
+    "stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, variant="fp16", use_safetensors=True
+)
+#pipe.save_pretrained("stabilityai_stable-diffusion-xl-base-1.0")
+torch.cuda.empty_cache()
+gc.collect()
+pipe.to("cuda")
+prompt = "Astronaut in a jungle, cold color palette, muted colors, detailed, 8k"
+image = pipe(prompt=prompt).images[0]
+image.save(f"astronaut_in_a_jungle.png")
+
+# Use refiner:
+from diffusers import DiffusionPipeline
+import torch
+base = DiffusionPipeline.from_pretrained(
+    "stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, variant="fp16", use_safetensors=True
+)
+base.to("cuda")
+refiner = DiffusionPipeline.from_pretrained(
+    "stabilityai/stable-diffusion-xl-refiner-1.0",
+    text_encoder_2=base.text_encoder_2,
+    vae=base.vae,
+    torch_dtype=torch.float16,
+    use_safetensors=True,
+    variant="fp16",
+)
+refiner.to("cuda")
+n_steps = 40
+high_noise_frac = 0.8
+prompt = "Astronaut in a jungle, cold color palette, muted colors, detailed, 8k"
+image = base(
+    prompt=prompt,
+    num_inference_steps=n_steps,
+    denoising_end=high_noise_frac,
+    output_type="latent",
+).images
+image = refiner(
+    prompt=prompt,
+    num_inference_steps=n_steps,
+    denoising_start=high_noise_frac,
+    image=image,
+).images[0]
+image.save(f"astronaut_in_a_jungle2.png")
+'|python
+
+
 # Whisper - test audio-transcription
 #-----------------------------------
 echo '
